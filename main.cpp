2d542f1094eba8c981824f18d72609d9d508c8e1
autumn-wind <junf_wang@126.com>
2021-11-09 10:32:44 +0800

    add way to time the sort algo

--- main.cpp
+++ main.cpp
@@ -10,7 +10,10 @@
 
 void print_usage_and_exit() {
 	printf("Wrong usage. Below are some right examples:\n\
-		./a.out test Insertion\n\
+		./a.out test Selection\n\
+		./a.out time Insertion\n\
+		./a.out time Insertion 100\n\
+		./a.out time Insertion 100 1000\n\
 		./a.out Insertion Selection\n\
 		./a.out Insertion Selection 100\n\
 		./a.out Insertion Selection 100 1000\n");
@@ -31,8 +34,7 @@ int main(int argn, char *argv[]) {
     if (alg1.compare("test") == 0 && it2 != algo_list.end() && argn == 3) {
 	Sort_Compare::test_sort_algo(alg2, it2->second);
     }
-    // compare two algorithms
-    else if (it1 != algo_list.end() && it2 != algo_list.end()) {
+    else {
 	int array_len = DEFAULT_ARRAY_LENGTH;
 	int loop_times = DEFAULT_LOOP_TIMES;
 	if (argn >= 4) {
@@ -50,15 +52,24 @@ int main(int argn, char *argv[]) {
 		print_usage_and_exit();
 	}
 
-	double t1 = Sort_Compare::sort_and_time_random_input(it1->second, array_len, loop_times);
-	double t2 = Sort_Compare::sort_and_time_random_input(it2->second, array_len, loop_times);
+	// time the algorithm
+	if (alg1.compare("time") == 0 && it2 != algo_list.end()) {
+	    double t = Sort_Compare::sort_and_time_random_input(it2->second, array_len, loop_times);
+	    printf("For %d random Ints of %d iteration\n%s (takes %f seconds)\n", 
+		    array_len, loop_times, alg2.c_str(), t);
+	}
+	// compare two algorithms
+	else if (it1 != algo_list.end() && it2 != algo_list.end()) {
+	    double t1 = Sort_Compare::sort_and_time_random_input(it1->second, array_len, loop_times);
+	    double t2 = Sort_Compare::sort_and_time_random_input(it2->second, array_len, loop_times);
 
-	printf("For %d random Ints of %d iteration\n\
-		%s (takes %f seconds) is %.3f times faster than %s (takes %f seconds)\n", 
-		array_len, loop_times, alg1.c_str(), t1, t2/t1, alg2.c_str(), t2);
+	    printf("For %d random Ints of %d iteration\n\
+		    %s (takes %f seconds) is %.3f times faster than %s (takes %f seconds)\n", 
+		    array_len, loop_times, alg1.c_str(), t1, t2/t1, alg2.c_str(), t2);
+	}
+	else
+	    print_usage_and_exit();
     }
-    else
-	print_usage_and_exit();
 
     return 0;
 }
